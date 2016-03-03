722db1f5b5a8f2fcc4772a739ebcf46b7b4657b9
northsal <narthsal@uw.edu>
2016-03-03 15:15:09 -0800

    Added first visual, still broken

--- statistics.js
+++ statistics.js
@@ -1,4 +1,56 @@
-function getData(d) {
-    console.log(d);
+function getData(data) {
+    var margin = {top: 40, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 510 - margin.top - margin.bottom;
+    var barPadding = 1;
+    var padding = 30;
+
+    var yScale = d3.scale.linear()
+        .domain([0, d3.max(data)])
+        .range([height - padding, padding]);
+    
+    var xScale = d3.scale.linear()
+        .domain([0, d3.max(data.length)])
+        .range([padding, width - padding * 2]);
+
+    var yAxis = d3.svg.axis()
+                    .scale(yScale)
+                    .orient("left");
+    var xAxis = d3.svg.axis()
+                    .scale(xScale)
+                    .orient("bottom");
+
+    var svg = d3.select("#chart")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+    svg.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i) {
+            return i * (width / data.length) + padding;
+        })
+        .attr("y", function(d) {
+            return yScale(d) - padding;
+        })
+        .attr("width", (width / data.length  - barPadding))
+        .attr("height", function(d) { 
+            return height - yScale(d);
+        });
+
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + padding + ",0)")
+        .call(yAxis);
+
+    svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(0," + (height - padding) + ")")
+                .call(xAxis);
+    
+
+ 
 }
 
